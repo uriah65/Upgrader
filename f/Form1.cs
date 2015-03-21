171dfe63ee8957cfb93c0a4a4363f15f66115d80
Uriah65 <uriah65@users.noreply.github.com>
2015-03-21 15:03:43 -0400

    .

---
+++ f/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace f
+{
+    public partial class Form1 : Form
+    {
+        public Form1(string[] args)
+        {
+            InitializeComponent();
+
+            string wrap = "Args: ";
+            foreach (string arg in args)
+            {
+                wrap += arg + "; ";
+            }
+
+            this.textBox1.Text = wrap;
+
+        }
+    }
+}
